381a2c0e724461d6af3c8dd68671760e20ba85b3
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-04-10 15:19:03 +0200

    Using pm2

--- src/hue.ts
+++ src/hue.ts
@@ -1,42 +1,51 @@
 import 'dotenv/config';
 import {Request, Response} from 'express';
 import {v3} from 'node-hue-api';
-import Api from 'node-hue-api/lib/api/Api';
-import LightState from 'node-hue-api/lib/model/lightstate/LightState';
-import {hex2RGB} from './utils';
+import {Api} from 'node-hue-api/dist/esm/api/Api';
+import {hex2XY} from './utils';
+const LightState = v3.lightStates.LightState;
 
 const {ROBERT_HUE_IP = '', ROBERT_HUE_USERNAME = ''} = process.env;
 
 type Light = {
-  id: number;
+  id: string;
   name: string;
   isReachable: boolean;
+  hasColor: boolean;
 };
 
 let bob: Api;
 let lights: Light[] = [];
 
 const fetchLights = async (): Promise<Light[]> =>
-  (await bob.lights.getAll()).map(({_data}) => ({
-    id: _data.id,
-    name: _data.name,
-    isReachable: _data.state.reachable,
+  (await bob.lights.getAll()).map(light => ({
+    id: light.id.toString(),
+    name: light.name,
+    //@ts-ignore fucking sloppy typing
+    isReachable: light.state.reachable ?? false,
+    hasColor: light.state.hasOwnProperty('hue'),
   }));
 
-const turnOn = (req: Request<{color: string}>, res: Response) => {
-  const [r, g, b] = hex2RGB(req.params.color);
-  const state = new LightState().on(true).brightness(100).rgb(r, g, b);
+const turnOn = ({params: {color}}: Request<{color: string}>, res: Response) => {
+  const onState = new LightState().on().brightness(100);
+  const colorState = new LightState().on().brightness(100).xy(hex2XY(color));
 
-  lights.forEach(light => bob.lights.setLightState(light.id, state));
+  lights.forEach(async ({id, hasColor}) => {
+    try {
+      await bob.lights.setLightState(id, hasColor ? colorState : onState);
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
   res.status(204).send();
 };
 
-const turnOff = (req: Request<{id?: string}>, res: Response) => {
+const turnOff = ({params: {id}}: Request<{id?: string}>, res: Response) => {
   const offState = new LightState().off();
 
-  if (req.params.id) {
-    bob.lights.setLightState(req.params.id, offState);
+  if (id) {
+    bob.lights.setLightState(id, offState);
   } else {
     lights.forEach(light => bob.lights.setLightState(light.id, offState));
   }
@@ -44,28 +53,35 @@ const turnOff = (req: Request<{id?: string}>, res: Response) => {
   res.status(204).send();
 };
 
-const toggle = async (req: Request<{id: string}>, res: Response) => {
-  const currentState = (await bob.lights.getLightState(req.params.id)) as {on: boolean};
+const toggle = async ({params: {id}}: Request<{id: string}>, res: Response) => {
+  const {on} = (await bob.lights.getLightState(id)) as {on: boolean};
 
-  bob.lights.setLightState(req.params.id, currentState.on ? new LightState().off() : new LightState().on(true));
+  bob.lights.setLightState(id, on ? new LightState().off() : new LightState().on());
 
-  res.json(!currentState.on);
+  res.json(!on);
 };
 
-const setBrightness = (req: Request<{ratio: string}>, res: Response) => {
-  lights.forEach(light => bob.lights.setLightState(light.id, {bri: Math.round(Number(req.params.ratio) * 2.54)}));
+const setBrightness = ({params: {ratio}}: Request<{ratio: number}>, res: Response) => {
+  const brightnessState = new LightState().brightness(ratio);
+
+  lights.forEach(async light => {
+    try {
+      await bob.lights.setLightState(light.id, brightnessState);
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
   res.status(204).send();
 };
 
-const getLights = async (_: Request, res: Response) => {
+const getLights = async (_req: Request, res: Response) => {
   lights = await fetchLights();
-
   res.json(lights);
 };
 
 (async () => {
-  bob = await v3.api.createInsecureLocal(ROBERT_HUE_IP).connect(ROBERT_HUE_USERNAME, false);
+  bob = await v3.api.createInsecureLocal(ROBERT_HUE_IP).connect(ROBERT_HUE_USERNAME);
   lights = await fetchLights();
 })();
 
--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,29 @@ const hex2RGB = (hex: string): number[] => {
   }
 };
 
+const RGB2XY = ([red, green, blue]: number[]): number[] => {
+  red = red > 0.04045 ? Math.pow((red + 0.055) / (1.0 + 0.055), 2.4) : red / 12.92;
+  green = green > 0.04045 ? Math.pow((green + 0.055) / (1.0 + 0.055), 2.4) : green / 12.92;
+  blue = blue > 0.04045 ? Math.pow((blue + 0.055) / (1.0 + 0.055), 2.4) : blue / 12.92;
+  //RGB values to XYZ using the Wide RGB D65 conversion formula
+  const X = red * 0.664511 + green * 0.154324 + blue * 0.162028;
+  const Y = red * 0.283881 + green * 0.668433 + blue * 0.047685;
+  const Z = red * 0.000088 + green * 0.07231 + blue * 0.986039;
+  //Calculate the xy values from the XYZ values
+  let x = Number((X / (X + Y + Z)).toFixed(4));
+  let y = Number((Y / (X + Y + Z)).toFixed(4));
+  if (isNaN(Number(x))) {
+    x = 0;
+  }
+  if (isNaN(Number(y))) {
+    y = 0;
+  }
+
+  return [x, y];
+};
+
+const hex2XY = (hex: string): number[] => RGB2XY(hex2RGB(hex));
+
 const sanitize = (string: string) => string.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 
-export {hex2RGB, sanitize};
+export {hex2RGB, sanitize, RGB2XY, hex2XY};
