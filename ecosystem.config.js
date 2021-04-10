158c529d790fb2f0abd4aa469d85abff32713c39
Valentin Dijkstra <valentindijkstra@gmail.com>
2021-04-10 15:58:03 +0200

    Same logfile

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'hk-server-node',
       script: 'build/index.js',
       combine_logs: true,
+      log_file: '/home/pi/.pm2/logs/hk-server-node.log',
       log_type: 'json',
     },
   ],
